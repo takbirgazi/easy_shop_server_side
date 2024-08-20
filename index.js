9392e1c624075d3296cff4ced2c486e4c1d3fca0
takbirgazi <takbirgazibd@gmail.com>
2024-08-20 17:15:48 +0600

    Install MongoDb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(express.json());
 app.use(cors());
