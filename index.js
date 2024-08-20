6867d08173be4b14d4aa9c9db79f33d4235df025
takbirgazi <takbirgazibd@gmail.com>
2024-08-20 17:16:42 +0600

    Setup MongoDB

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.mbbwdlc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 
 app.get("/", (req, res) => {
